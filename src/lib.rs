2c761360061c2df21a49226b22fc3c092fa199d4
jkobejs <josip.grgurica@gmail.com>
2020-05-24 18:49:34 +0200

    trait

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,18 @@
 mod tests {
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+       assert_eq!(2 + 2, 4);
     }
 }
+
+trait QueryString<T> {
+    fn decode(query_string: &str) -> Option<T>;
+    fn encode(t: &T) -> &str;
+
+    // ?size=1&name=josip (1, "josip")
+    // fn combine<V>(&self, &qs: QueryString<V>) -> &QueryString<(V, T)>;
+}
+
+
+
+
